c775af0f26ce98dbf3a3b696fff941ecbe589cb7
Don Ky <don.d.ky@gmail.com>
2019-12-24 16:43:36 -0500

    tweak comments

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #
-# A Pomodoro timer
+# A Pomodoro timer - Because productivity...
 # Underlying principles
 # There are six steps in the original technique:
-
 # 1. Decide on the task to be done.
 # 2. Set the pomodoro timer (traditionally to 25 minutes).
 # 3. Work on the task.
 # 4. End work when the timer rings and put a checkmark on a piece of paper.[6]
 # 5. If you have fewer than four checkmarks, take a short break (3–5 minutes), then go to step 2.
-
-# After four pomodoros, take a longer break (15–30 minutes), reset your checkmark count to zero, then go to step 1.
+# 6. After four pomodoros, take a longer break (15–30 minutes), reset your checkmark count to zero, then go to step 1.
 
 work_seconds=${1:-25}*60; # 1500 seconds
 break_seconds=${2:-work_seconds/300}*60; # 300 seconds
