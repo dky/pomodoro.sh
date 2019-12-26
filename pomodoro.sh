260782f2dbf36c4439403b3d9a3d7df32bad2e0d
Don Ky <don.d.ky@gmail.com>
2019-12-26 10:14:58 -0500

    Replace all occurances of the date command with DATE_CMD

--- pomodoro.sh
+++ pomodoro.sh
@@ -2,6 +2,8 @@
 #
 # A Pomodoro timer - Because productivity...
 
+DATE_CMD=gdate
+
 notify() {
 	notification=$1
 	duration=$2
@@ -23,15 +25,15 @@ notify() {
 
 count_down() {
 	seconds=$1
-	#echo "seconds_to_count: $seconds, epoc_now: $(date +%s), Now - Seconds = $(expr $seconds - $(date +%s))"
-	echo -ne "$(date -u -j -f %s $(($seconds - $(date +%s))) +%H:%M:%S)\r";
+	#echo "seconds_to_count: $seconds, epoc_now: $($DATE_CMD +%s), Now - Seconds = $(expr $seconds - $($DATE_CMD +%s))"
+	echo -ne "$($DATE_CMD -u -j -f %s $(($seconds - $($DATE_CMD +%s))) +%H:%M:%S)\r";
 }
 
 log_work() {
 	task="$1"
 	duration=$2
 	work_log=./pomodoro.log
-	work_date=`date +%Y-%m-%dT%H:%M:%SZ`
+	work_date=`$DATE_CMD +%Y-%m-%dT%H:%M:%SZ`
 
 	if [[ ! -e $work_log ]]; then
 		touch $work_log
@@ -41,29 +43,30 @@ log_work() {
 
 }
 
-
 main () {
 	task="${1:-codebreakers}";
 	wseconds=${2:-25}*60; # work seconds, default is 25 mins.
 	pseconds=${3:-wseconds/300}*60; # pause/break seconds, default is 5 mins
 
 	while true; do
-		wseconds_epoch=$((`date +%s` + $wseconds));
+		wseconds_epoch=$((`$DATE_CMD +%s` + $wseconds));
 		pomodoro_duration=$((wseconds / 60));
 
-		while [ "$wseconds_epoch" -ge `date +%s` ]; do
-			echo -ne "$(gdate -u --date @$(($wseconds_epoch - `date +%s` )) +%H:%M:%S)\r";
+		echo "This is the task we are working on: $task"
+
+		while [ "$wseconds_epoch" -ge `$DATE_CMD +%s` ]; do
+			echo -ne "$($DATE_CMD -u --date @$(($wseconds_epoch - `$DATE_CMD +%s` )) +%H:%M:%S)\r";
 		done
 
 		notify pomodoro_complete $pomodoro_duration;
 		read -n1 -rsp $'Press any key to take a break or Ctrl+C to exit...\n';
 		log_work $task $pomodoro_duration;
 
-		pseconds_epoch=$((`date +%s` + $pseconds));
+		pseconds_epoch=$((`$DATE_CMD +%s` + $pseconds));
 		break_duration=$((pseconds / 60));
 
-		while [ "$pseconds_epoch" -gt `date +%s` ]; do
-			echo -ne "$(gdate -u --date @$(($pseconds_epoch - `date +%s` )) +%H:%M:%S)\r";
+		while [ "$pseconds_epoch" -gt `$DATE_CMD +%s` ]; do
+			echo -ne "$($DATE_CMD -u --date @$(($pseconds_epoch - `$DATE_CMD +%s` )) +%H:%M:%S)\r";
 		done
 
 		notify short_break_complete $break_duration;
