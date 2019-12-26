1638d271bc9813a7fc327919be2880018e950eb8
Don Ky <don.d.ky@gmail.com>
2019-12-26 15:55:36 -0500

    Add some ansi colors

--- pomodoro.sh
+++ pomodoro.sh
@@ -5,17 +5,24 @@
 # Use coreutils gdate on OSX, go away BSD date...
 DATE_CMD=gdate
 
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[01;33m'
+CYAN='\033[01;36m'
+
+NC='\033[0m' # No Color
+
 notify() {
 	notification=$1
 	duration=$2
 
 	if [ "$notification"  == "pomodoro_complete" ]; then
-		notification_message="$duration minute Pomodoro done! Time to take a quick break and log progress."
+		notification_message="${YELLOW}$duration${NC} minute ${RED}pomodoro${NC} done! Time to take a quick ${YELLOW}5${NC} min break and log progress."
 		osascript -e 'display notification "Time to take a quick break" with title "Pomodoro Complete"';
 		say -v kyoko "やった"
 	elif [ "$notification" == "short_break_complete" ]; then
-		notification_message="$duration minute break done! Let's get ready to crush another one!"
-		osascript -e 'display notification "Time to get back to work" with title "Lets do another one!"';
+		notification_message="${YELLOW}$duration${NC} minute break done! Let's get ready to crush another one!"
+		osascript -e 'display notification "Time to get back to work" with title "Lets complete another Pomodoro!"';
 	else {
 		notification_message="15 minute break done!"
 	}
@@ -43,7 +50,6 @@ log_work() {
 	fi
 
 	echo "$work_date,$task,$duration,$count" >> $work_log
-
 }
 
 
@@ -55,17 +61,17 @@ main () {
 	#break_seconds=3 # 3 seconds for testing.
 	# Log the break type as long or short, useful for log analysis
 	break_type=short
-	# How many Pomodoro's done this session, resets to zero at 4.
+	# How many pomodoros done this session, resets to zero at 4.
 	pomodoro_count=0
 
 	while true; do
 		pomodoro_seconds_epoch=$((`$DATE_CMD +%s` + $pomodoro_seconds));
 		pomodoro_duration=$((pomodoro_seconds / 60));
 
-		printf "Currently working on: $pomodoro_name, Pomodoros completed for $pomodoro_name: $pomodoro_count this session\n\n"
+		printf "Currently working on: ${CYAN}$pomodoro_name${NC}, ${RED}pomodoros${NC} completed for ${CYAN}$pomodoro_name${NC}: ${YELLOW}$pomodoro_count${NC} this session\n\n"
 
 		while [ "$pomodoro_seconds_epoch" -ge `$DATE_CMD +%s` ]; do
-			echo -ne "Time left in this Pomodoro: $($DATE_CMD -u --date @$(($pomodoro_seconds_epoch - `$DATE_CMD +%s` )) +%H:%M:%S)\r";
+			echo -ne "Time left in this pomodoro: ${GREEN}$($DATE_CMD -u --date @$(($pomodoro_seconds_epoch - `$DATE_CMD +%s` )) +%H:%M:%S)\r${NC}";
 			sleep 0.1 # Sleep for 1/10th of a second to not kill cpu.
 		done
 
@@ -77,26 +83,27 @@ main () {
 			pomodoro_count=0
 			break_type=long
 
-			printf "Awesome Job! You just completed: 4 pomodoros for $pomodoro_name, time for a well deserved long break!\n"
-			printf "Whatever you do, don't stare at the screen for 30 mins!\n"
-			printf "Resetting Pomodoro counts for $pomodoro_name, let's do another 4 after this break!\n"
+			printf "Awesome Job! You just completed: ${YELLOW}4${NC} ${RED}pomodoros${NC} for ${CYAN}$pomodoro_name${NC}\n";
+			printf "Time for a well deserved ${YELLOW}30${NC} min break!\n";
+			printf "Whatever you do, don't stare at the screen for ${YELLOW}30${NC} mins!\n";
+			printf "Resetting ${RED}pomodoro${NC} counts for ${CYAN}$pomodoro_name${NC}, let's do another ${YELLOW}4${NC} after this break!\n";
 		else
 			notify pomodoro_complete $pomodoro_duration;
 		fi
 
-		read -n1 -rsp $'Press any key to take a break or Ctrl+C to exit...\n';
+		read -n1 -rsp $'Press \e[36many key\e[0m to take the \e[35mbreak\e[0m or \e[36mCtrl+C\e[0m to exit...\n\n';
 		log_work $pomodoro_name $pomodoro_duration $pomodoro_count;
 
 		break_seconds_epoch=$((`$DATE_CMD +%s` + $break_seconds));
 		break_duration=$((break_seconds / 60));
 
 		while [ "$break_seconds_epoch" -gt `$DATE_CMD +%s` ]; do
-			echo -ne "Time left in Break: $($DATE_CMD -u --date @$(($break_seconds_epoch - `$DATE_CMD +%s` )) +%H:%M:%S)\r";
+			echo -ne "Time left in Break: ${GREEN}$($DATE_CMD -u --date @$(($break_seconds_epoch - `$DATE_CMD +%s` )) +%H:%M:%S)\r${NC}";
 			sleep 0.1 # Sleep for 1/10th of a second to not kill cpu.
 		done
 
 		notify short_break_complete $break_duration;
-		read -n1 -rsp $'Press any key to start another Pomodoro or Ctrl+C to exit...\n';
+		read -n1 -rsp $'Press \e[36many key\e[0m to \e[35mstart another\e[0m \e[31mpomodoro\e[0m or \e[36mCtrl+C\e[0m to exit...\n\n';
 		log_work $break_type $break_duration 1;
 	done
 }
