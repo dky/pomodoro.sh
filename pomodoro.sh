b08d5bd501bca79ff564a6986d5f63ba10911e17
Don Ky <don.d.ky@gmail.com>
2019-12-27 08:43:21 -0500

    Color pomo red

--- pomodoro.sh
+++ pomodoro.sh
@@ -81,7 +81,7 @@ main () {
 		printf "Currently working on: ${CYAN}$pomodoro_name${NC}, ${RED}pomodoros${NC} completed for: ${CYAN}$pomodoro_name${NC}: ${YELLOW}$pomodoro_count${NC} this session\n\n"
 
 		while [ "$pomodoro_seconds_epoch" -ge `$DATE_CMD +%s` ]; do
-			echo -ne "Time left in this pomodoro: ${GREEN}$($DATE_CMD -u --date @$(($pomodoro_seconds_epoch - `$DATE_CMD +%s` )) +%H:%M:%S)\r${NC}";
+			echo -ne "Time left in this ${RED}pomodoro${NC}: ${GREEN}$($DATE_CMD -u --date @$(($pomodoro_seconds_epoch - `$DATE_CMD +%s` )) +%H:%M:%S)\r${NC}";
 			sleep 1 # Sleep for 1 second to not kill cpu.
 		done
 
